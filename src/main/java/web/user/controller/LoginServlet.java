42122c6c1ddd2f6a009dd35b075eaafab8c85378
RichJhang <volleyball810061@gmail.com>
2025-10-19 09:26:19 +0800

    user

--- src/main/java/web/user/controller/LoginServlet.java
+++ src/main/java/web/user/controller/LoginServlet.java
@@ -1,48 +1,41 @@
 package web.user.controller;
 
 import static core.util.CommonUtil.json2Pojo;
-import static core.util.CommonUtil.writePojo2Json;
-import static core.util.UserConstants.SERVICE;
 
 import java.io.IOException;
 
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
+import com.google.gson.JsonObject;
 
 import web.user.pojo.User;
-	
+import web.user.service.UserService;
+import web.user.service.impl.UserServiceImpl;
+
 @WebServlet("/user/login")
 public class LoginServlet extends HttpServlet {
-
 	private static final long serialVersionUID = 1L;
+	private UserService userService;
+
+	@Override
+	public void init() throws ServletException {
+		try {
+			userService = new UserServiceImpl();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		User user = json2Pojo(req, User.class);
-		if (user == null) {
-			user = new User();
-			user.setMessage("無會員資訊");
-			user.setSuccessful(false);
-			writePojo2Json(resp, user);
-			return;
-		}
-
-		user = SERVICE.login(user);
-		if (user.isSuccessful()) {
-			if (req.getSession(false) != null) {
-				req.changeSessionId();
-			}
-			final HttpSession session = req.getSession();
-			session.setAttribute("loggedin", true);
-			session.setAttribute("user", user);
-			System.out.println(user);
-		}
-		writePojo2Json(resp, user);
-
+		JsonObject obj = new JsonObject();
+		User message = userService.login(user);
+		obj.add("", obj);
 	}
-
 }
