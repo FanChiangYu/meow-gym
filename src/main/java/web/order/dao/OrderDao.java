7b47292770ea5eaf6c081dc2b2aa8307f5518636
JOANNA\CYLIZZC <joannali60123@gmail.com>
2025-10-27 19:53:30 +0800

    [Order]Orders/Orders(.java/.html/.css/.js)/套前端, 改成spring

--- src/main/java/web/order/dao/OrderDao.java
+++ src/main/java/web/order/dao/OrderDao.java
@@ -1,10 +1,21 @@
 package web.order.dao;
 
+import java.util.List;
+
 import core.dao.CoreDao;
+import web.order.pojo.Orderitems;
 import web.order.pojo.Orders;
 
 public interface OrderDao extends CoreDao<Orders, Integer>{
 	
+	Orders selectById(Integer id);
+	
 	int insert(Orders orders);
-
+	
+	int insert(Orderitems orderitems);
+	
+	Integer selectOrderIdByUesrIdAndStatus(Integer userId, String status);
+	
+	Integer selectCoursePriceByCourseId(Integer courseId);
+	
 }
