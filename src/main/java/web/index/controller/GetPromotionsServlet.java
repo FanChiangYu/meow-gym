e11346e45353066a4a546e5ff79b8f3c6d05a82a
Fan <energy299@hotmail.com>
2025-11-01 22:20:45 +0800

    [index] / 首頁初版

--- src/main/java/web/index/controller/GetPromotionsServlet.java
+++ src/main/java/web/index/controller/GetPromotionsServlet.java
@@ -1,5 +1,33 @@
 package web.index.controller;
 
-public class GetPromotionsServlet {
+import static core.util.CommonUtil.writePojo2Json;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import core.util.CommonUtil;
+import web.index.service.IndexService;
+import web.promotions.pojo.CoursePromo;
+
+@WebServlet("/index/getPromotions")
+public class GetPromotionsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private IndexService service;
+
+	@Override
+	public void init() throws ServletException {
+		service = CommonUtil.getBean(getServletContext(), IndexService.class);
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		List<CoursePromo> cpList = service.findAllPromo();
+		writePojo2Json(resp, cpList);
+	}
 }
--- src/main/java/web/index/service/impl/IndexServiceImpl.java
+++ src/main/java/web/index/service/impl/IndexServiceImpl.java
@@ -1,7 +1,46 @@
 package web.index.service.impl;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import web.index.dao.IndexDao;
 import web.index.service.IndexService;
+import web.promotions.pojo.CoursePromo;
 
+@Service
+@Transactional
 public class IndexServiceImpl implements IndexService {
+	@Autowired
+	private IndexDao dao;
 
+	@Override
+	public List<CoursePromo> findAllPromo() {
+		List<CoursePromo> cpAllList = dao.selectAll();
+		List<CoursePromo> cpCurList = new ArrayList<>();
+		if (!cpAllList.isEmpty()) {
+			for (CoursePromo cp : cpAllList) {
+				if (isOnSale(cp)) {
+					cpCurList.add(cp);
+				}
+			}
+			return cpCurList;
+		}
+		return cpAllList;		
+	}
+
+	@Override
+	public Boolean isOnSale(CoursePromo coursePromo) {
+		Date today = new Date();
+		if (today.after(coursePromo.getDateStart()) && today.before(coursePromo.getDateEnd())) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 }
