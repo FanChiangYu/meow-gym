5f50c2d3754b6888124a3de2a1fc766cb593d6e4
RichJhang <volleyball810061@gmail.com>
2025-11-22 17:05:29 +0800

    [user] code 排版

--- src/main/webapp/user/js/resetPassword.js
+++ src/main/webapp/user/js/resetPassword.js
@@ -5,11 +5,11 @@ const password = document.querySelector('#password');
 const cPassword = document.querySelector('#confirm-password');
 
 function valueOrNull(value) {
-	if (value === undefined || value === null || value === '' || Number.isNaN(value)) {
-		return null;
-	} else {
-		return value;
-	}
+  if (value === undefined || value === null || value === '' || Number.isNaN(value)) {
+    return null;
+  } else {
+    return value;
+  }
 }
 
 fetch('checkUser', {
@@ -19,44 +19,44 @@ fetch('checkUser', {
     userId: sessionStorage.getItem('userId')
   }),
 })
-.then(resp => resp.json())
-.then(respbody => {
-  if(!respbody.checkStatus){
-    location.href = '/meow-gym/user/forgotPassword.html';
-  }
-});
+  .then(resp => resp.json())
+  .then(respbody => {
+    if (!respbody.checkStatus) {
+      location.href = '/meow-gym/user/forgotPassword.html';
+    }
+  });
 
-setBtn.addEventListener('click', function() {
+setBtn.addEventListener('click', function () {
 
   if (valueOrNull(password.value) === null) {
     Swal.fire({
       title: '錯誤',
       text: '新密碼未輸入',
       icon: 'error',
-      target: document.body 
+      target: document.body
     });
-		return;
-	}
+    return;
+  }
 
   if (valueOrNull(cPassword.value) === null) {
     Swal.fire({
       title: '錯誤',
       text: '確認密碼未輸入',
       icon: 'error',
-      target: document.body 
+      target: document.body
     });
     return;
-	}
+  }
 
   if (valueOrNull(cPassword.value) !== valueOrNull(password.value)) {
     Swal.fire({
       title: '錯誤',
       text: '新密碼與確認密碼不相同',
       icon: 'error',
-      target: document.body 
+      target: document.body
     });
     return;
-	}
+  }
 
   fetch('change', {
     method: 'POST',
@@ -66,24 +66,23 @@ setBtn.addEventListener('click', function() {
       password: valueOrNull(password.value)
     }),
   })
-  .then(resp => resp.json())
-  .then(respbody => {
-    if(respbody.successful){
-      Swal.fire({
-        title: '完成',
-        text: '密碼已變更',
-        icon: 'success',
-        target: document.body 
-      })
-      .then(()=>location.href = '/meow-gym/index/index.html');
-    }else{
-      Swal.fire({
-        title: '錯誤',
-        text: '新密碼與舊密碼相同',
-        icon: 'error',
-        target: document.body 
-      });
-    }
-  });
-
+    .then(resp => resp.json())
+    .then(respbody => {
+      if (respbody.successful) {
+        Swal.fire({
+          title: '完成',
+          text: '密碼已變更',
+          icon: 'success',
+          target: document.body
+        })
+          .then(() => location.href = '/meow-gym/index/index.html');
+      } else {
+        Swal.fire({
+          title: '錯誤',
+          text: '新密碼與舊密碼相同',
+          icon: 'error',
+          target: document.body
+        });
+      }
+    });
 });
