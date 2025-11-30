717e928d7e04f5d4f96bc69148054e7b57ba5257
Fan <energy299@hotmail.com>
2025-11-30 23:12:55 +0800

    [index] 移除未使用import

--- src/main/java/web/index/controller/IndexController.java
+++ src/main/java/web/index/controller/IndexController.java
@@ -3,10 +3,8 @@ package web.index.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import web.index.service.IndexService;
