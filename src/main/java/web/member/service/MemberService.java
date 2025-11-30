82d6924648d9bce0038b14538a2f775d7716b3ea
Fan <energy299@hotmail.com>
2025-11-30 23:06:12 +0800

    [other] 刪除無用資料

--- src/main/java/web/member/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package web.member.service;
-
-import web.member.pojo.Member;
-
-public interface MemberService {
-
-	Member login(Member member);
-	
-}
--- src/main/java/web/review/service/impl/ReviewServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package web.review.service.impl;
-
-import web.review.service.ReviewService;
-
-public class ReviewServiceImpl implements ReviewService{
-
-}
